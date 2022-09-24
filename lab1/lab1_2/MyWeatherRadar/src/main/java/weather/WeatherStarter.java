04fb5e5d74238b4dbe3a8c63111ff61dab29215b
Guilherme Lopes <gui.lopes@ua.pt>
2022-09-24 19:36:00 +0100

    implement logger

--- lab1/lab1_2/MyWeatherRadar/src/main/java/weather/WeatherStarter.java
+++ lab1/lab1_2/MyWeatherRadar/src/main/java/weather/WeatherStarter.java
@@ -1,5 +1,8 @@
 package weather;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -14,6 +17,8 @@ public class WeatherStarter {
 
     //todo: should generalize for a city passed as argument
     private static final int ID = 1010500;
+
+    protected static final Logger logger = LogManager.getLogger();
 
     public static void  main(String[] args ) {
 
@@ -37,9 +42,8 @@ public class WeatherStarter {
             if (forecast != null) {
                 var firstDay = forecast.getData().listIterator().next();
 
-                System.out.printf( "max temp for %s is %4.1f %n",
-                        firstDay.getForecastDate(),
-                        Double.parseDouble(firstDay.getTMax()));
+                System.out.printf( "max temp for %s is %4.1f %n",firstDay.getForecastDate(),Double.parseDouble(firstDay.getTMax()));
+                logger.info("Max temp for" + firstDay.getForecastDate() + " is "+ Double.parseDouble(firstDay.getTMax()));
             } else {
                 System.out.println( "No results for this request!");
             }
